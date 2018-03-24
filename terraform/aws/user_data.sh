cdf71365383f843fb0c8c9773d21703ee4272aeb
Michael Dance <michael.dance@gmail.com>
2018-03-24 14:25:07 -0600

    Updated terraform/aws/user_data.sh to create /etc/ansible/hosts

--- terraform/aws/user_data.sh
+++ terraform/aws/user_data.sh
@@ -1,6 +1,17 @@
 #!/bin/bash
+
+ANSIBLE_HOSTS=/etc/ansible/hosts
+
 apt-get update
 apt-get -y install python-pip
 pip install ansible
-HOME=/root ansible-pull -o -d /opt/bitcoin-node -U https://github.com/mdance/bitcoin-node /opt/bitcoin-node/ansible/bitcoin_node.yml >> /var/log/ansible-pull.log
 
+mkdir -p /etc/ansible
+
+if [ ! -e $ANSIBLE_HOSTS ]; then
+  cat >${ANSIBLE_HOSTS} <<EOF
+localhost
+EOF
+fi
+
+HOME=/root ansible-pull -o -d /opt/bitcoin-node -U https://github.com/mdance/bitcoin-node /opt/bitcoin-node/ansible/bitcoin_node.yml >> /var/log/ansible-pull.log

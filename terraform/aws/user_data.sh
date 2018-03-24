66ce0e7dc7346ce4fdb56add0d82d493a85c10c9
Michael Dance <michael.dance@gmail.com>
2018-03-24 16:13:07 -0600

    Added .env file creation to user_data.sh

--- terraform/aws/user_data.sh
+++ terraform/aws/user_data.sh
@@ -13,4 +13,23 @@ localhost
 EOF
 fi
 
+ENV_FILE=$HOME/.env
+
+if [ ! -e $ENV_FILE ]; then
+	PASSWORD=`openssl rand -hex 32`
+
+  cat >${ENV_FILE} <<EOF
+BTC_PRUNE=0
+BTC_RPCUSER=btc
+BTC_RPCPASSWORD=$PASSWORD
+BTC_RPCCLIENTTIMEOUT=30
+BTC_RPCALLOWIP=::/0
+BTC_RPCPORT=8332
+BTC_PRINTTOCONSOLE=1
+BTC_DISABLEWALLET=0
+BTC_TXINDEX=1
+BTC_TESTNET=0
+EOF
+fi
+
 HOME=/root ansible-pull -o -d /opt/bitcoin-node -U https://github.com/mdance/bitcoin-node /opt/bitcoin-node/ansible/bitcoin_node.yml >> /var/log/ansible-pull.log
